938449a481a1137c9539379322164c25c32315e8
BlackDude22 <lukislt7@gmail.com>
2020-01-13 17:55:12 +0200

    added stuff

--- bce/BitcoinExplorer/urls.py
+++ bce/BitcoinExplorer/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('BlockExplorer/', views.blockExplorer, name='be')
+    # path('BlockExplorer/', views.blockExplorer, name='be')
+    path('<int:block_height>/', views.details, name='details'),
 ]
--- bce/BitcoinExplorer/views.py
+++ bce/BitcoinExplorer/views.py
@@ -3,13 +3,24 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    latest_question_list = ["1", "2", "suka"]
+    latest_blocks = [["1", "hash1"], ["2", "hash2"]]
     template = loader.get_template('BitcoinExplorer/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_blocks': latest_blocks,
     }
-    return render(request, 'BitcoinExplorer/be.html', {})
+    return render(request, 'BitcoinExplorer/index.html', context)
 
-def blockExplorer(request):
-	context = {}
-	return render(request, 'BitcoinExplorer/be.html', context)
+def details(request, block_height):
+    block = ["height", "hash", "previous_hash","time", "confirmations", "nonce", "total_transactions", "total_transacted", "total_fee"]
+    context = {
+    	'height': block[0],
+    	'hash': block[1],
+    	'previous_hash': block[2],
+    	'time': block[3],
+    	'confirmations': block[4],
+    	'nonce': block[5],
+    	'total_transactions': block[6],
+    	'total_transacted': block[7],
+    	'total_fee': block[8],
+    }
+    return render(request, 'BitcoinExplorer/block.html', context)
